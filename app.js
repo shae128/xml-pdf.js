f368c1c26ba2bdfc0166d7e6600e022ebad441cf
shae128 <slr772s@gmail.com>
2020-09-16 17:41:12 +0200

    Added express and Codice di Plavicino to project

---
+++ app.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+
+// *****************************
+// *********Reading files*******
+// *****************************
+
+let filesName = [];
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'public/CodicePelavicino');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+      //push files name to it's array
+      filesName.push(file)
+    });
+});
+
+// *****************************
+
+
+//app.use((req, res, next) => {
+//  console.log('new request made:');
+//  console.log('host: ', req.hostname);
+//  console.log('path: ', req.path);
+//  console.log('method: ', req.method);
+//  next();
+//});
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home', filesName });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
